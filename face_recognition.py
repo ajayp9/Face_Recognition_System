1a65397778e9108dc2e95fb5804c23688a0d92fd
Ajay Pal <124481153+ajayp9@users.noreply.github.com>
2024-12-19 22:50:41 +0530

    Update face_recognition.py

--- face_recognition.py
+++ face_recognition.py
@@ -128,10 +128,10 @@ class Face_Recognition:
          
 
           if confidence>60:
-            cv2.putText(img,f"ID:{i}",(x,y-75),cv2.FONT_HERSHEY_COMPLEX,0.8,(255,255,255),3)
-            cv2.putText(img,f"Roll:{r}",(x,y-55),cv2.FONT_HERSHEY_COMPLEX,0.8,(255,255,255),3)
-            cv2.putText(img,f"Name:{n}",(x,y-30),cv2.FONT_HERSHEY_COMPLEX,0.8,(255,255,255),3)
-            cv2.putText(img,f"Department:{d}",(x,y-5),cv2.FONT_HERSHEY_COMPLEX,0.8,(255,255,255),3)
+            cv2.putText(img,f"ID:{i}",(x,y-75),cv2.FONT_HERSHEY_COMPLEX,0.8,(0,122,255),3)
+            cv2.putText(img,f"Roll:{r}",(x,y-55),cv2.FONT_HERSHEY_COMPLEX,0.8,(0,122,255),3)
+            cv2.putText(img,f"Name:{n}",(x,y-30),cv2.FONT_HERSHEY_COMPLEX,0.8,(0,122,255),3)
+            cv2.putText(img,f"Department:{d}",(x,y-5),cv2.FONT_HERSHEY_COMPLEX,0.8,(0,122,255),3)
             self.mark_attendance(i,r,n,d)
           else:
             cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),3)
